fef1f2816d78d8912658ffe3d29252d964619ce9
modestas.kazinauskas <modestas.kazinauskas@gmail.com>
2020-06-11 23:17:02 +0300

    border removal

--- src/pages/radioStations/streams/RadioStationStreamsHeader.js
+++ src/pages/radioStations/streams/RadioStationStreamsHeader.js
@@ -23,9 +23,6 @@ class RadioStationStreamsHeader extends Component {
     render() {
         return (
             <PageHeader
-                style={{
-                    border: '1px solid rgb(235, 237, 240)',
-                }}
                 onBack={this.handleBack}
                 title="Radio station streams"
             />
